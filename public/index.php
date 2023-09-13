57bd16ac90dab37cb1ab40410ee598000d7446c8
Tejas Saraf <111191673+TejasSaraf@users.noreply.github.com>
2023-09-13 20:48:40 +0530

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,10 +1,7 @@
 <?php
-
 use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
-
 define('LARAVEL_START', microtime(true));
-
 /*
 |--------------------------------------------------------------------------
 | Check If The Application Is Under Maintenance
@@ -45,11 +42,8 @@ require __DIR__.'/../vendor/autoload.php';
 */
 
 $app = require_once __DIR__.'/../bootstrap/app.php';
-
 $kernel = $app->make(Kernel::class);
-
 $response = $kernel->handle(
     $request = Request::capture()
 )->send();
-
 $kernel->terminate($request, $response);
