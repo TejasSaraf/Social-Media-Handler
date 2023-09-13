1c79a8adc89072671748ecda470db72f52053eb8
Tejas Saraf <111191673+TejasSaraf@users.noreply.github.com>
2023-09-13 20:47:35 +0530

    Update Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -6,13 +6,6 @@ use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
 {
-    /**
-     * The application's global HTTP middleware stack.
-     *
-     * These middleware are run during every request to your application.
-     *
-     * @var array<int, class-string|string>
-     */
     protected $middleware = [
         // \App\Http\Middleware\TrustHosts::class,
         \App\Http\Middleware\TrustProxies::class,
